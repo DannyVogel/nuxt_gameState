b601e1b6ff36a2ffe1fe5e8836d4011122e14c49
Danny <danny.hj.vogel@gmail.com>
2024-06-19 01:01:23 +0200

    Fix: class

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -3,9 +3,9 @@
 <template>
   <div class="flex justify-center border-b border-gray-600">
     <h1
-      className="py-1 font-bold text-4xl mx-auto bg-gradient-to-l from-fuchsia-500 via-red-600 to-orange-400 bg-clip-text text-transparent"
+      class="py-1 font-bold text-4xl mx-auto bg-gradient-to-l from-fuchsia-500 via-red-600 to-orange-400 bg-clip-text text-transparent"
     >
-      <NuxtLink to="/"> <span className="italic">game</span>State </NuxtLink>
+      <NuxtLink to="/"> <span class="italic">game</span>State </NuxtLink>
     </h1>
   </div>
 </template>

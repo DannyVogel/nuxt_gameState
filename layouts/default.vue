9fa90fbc87a07663b4017f2e1cbb70ddb860af44
Danny <danny.hj.vogel@gmail.com>
2024-06-19 01:01:32 +0200

    Feat: init basic layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="container min-h-screen flex flex-col">
+  <div class="container mx-auto min-h-screen grid grid-rows-[56px,1fr,72px]">
     <TheHeader />
-    <div class="flex-grow">
+    <div class="">
       <slot />
     </div>
     <TheFooter />

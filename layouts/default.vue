e8a4b2b10f7405b746c6572d6271c104b84084ec
Danny <danny.hj.vogel@gmail.com>
2024-06-20 00:43:33 +0200

    WIP: layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="container mx-auto min-h-screen grid grid-rows-[56px,1fr,72px]">
+  <div
+    class="container mx-auto min-h-screen max-h-screen grid grid-rows-[56px,1fr,72px]"
+  >
     <TheHeader />
-    <div class="">
+    <div class="p-4 overflow-x-hidden max-h-full">
       <slot />
     </div>
     <TheFooter />

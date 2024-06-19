45e48e9a24a4476551ae09a87a5be326cf7e18d8
Danny <danny.hj.vogel@gmail.com>
2024-06-20 00:13:14 +0200

    Feat: init search/[query] page

---
+++ pages/search/[query].vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+// @ts-ignore
+import { AtomSpinner } from "epic-spinners";
+import type { Game } from "~/types/game.interfaces";
+const route = useRoute();
+const { search } = useSearch();
+const isLoding = ref(true);
+const results = ref<Game[]>([]);
+
+onMounted(async () => {
+  results.value = await search(route.params.query as string);
+  isLoding.value = false;
+});
+</script>
+
+<template>
+  <div class="min-h-full flex flex-col items-center justify-center">
+    <atom-spinner
+      v-if="isLoding"
+      :animation-duration="1000"
+      :size="100"
+      :color="'rgb(59 130 246 / 0.5)'"
+    />
+    <template v-else>
+      <div v-if="results.length === 0">
+        <p class="text-primary">
+          No results found for "{{ route.params.query }}"
+        </p>
+      </div>
+      <div v-else class="flex flex-col gap-4">
+        <h1 class="text-xl font-bold text-primary text-center">
+          {{ results.length }} Results for "{{ route.params.query }}"
+        </h1>
+        <template v-for="game in results">
+          <GameCard :game="game" />
+        </template>
+
+        <div class="grid grid-cols-1 gap-4"></div>
+      </div>
+    </template>
+  </div>
+</template>

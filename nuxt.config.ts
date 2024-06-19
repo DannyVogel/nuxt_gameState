a76b80a4d0336551b462546278c9f54c5fdc4fcf
Danny <danny.hj.vogel@gmail.com>
2024-06-19 19:04:11 +0200

    Fix: add IGDB keys; move firebase keys to env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,16 +7,17 @@ export default defineNuxtConfig({
   },
   modules: ["@nuxt/ui", "@nuxt/fonts", "@pinia/nuxt"],
   runtimeConfig: {
+    IGDBClientId: "",
+    IGDBClientSecret: "",
     public: {
       firebase: {
-        apiKey: "AIzaSyDvZtRn9B9OgWOIVHnICiq0_eoNreJBUs0",
-        authDomain: "AIzaSyDvZtRn9B9OgWOIVHnICiq0_eoNreJBUs0",
-        projectId: "gamestate-327e2",
-        appId: "1:168937433606:web:0919320d20ca2f94033187",
-        databaseURL:
-          "https://gamestate-327e2-default-rtdb.europe-west1.firebasedatabase.app",
-        storageBucket: "gamestate-327e2.appspot.com",
-        messagingSenderId: "168937433606",
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        appId: "",
+        databaseURL: "",
+        storageBucket: "",
+        messagingSenderId: "",
       },
     },
   },

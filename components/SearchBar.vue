6edeb11d3ea8972126bd4eb51f9bcd0f4eccc13c
Danny <danny.hj.vogel@gmail.com>
2024-06-19 08:33:24 +0200

    Feat: init search bar

---
+++ components/SearchBar.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+const search = ref("");
+</script>
+
+<template>
+  <div class="max-w-xs mx-auto">
+    <UInput
+      v-model="search"
+      name="q"
+      placeholder="Search..."
+      icon="i-heroicons-magnifying-glass-20-solid"
+      autocomplete="off"
+      :ui="{ icon: { trailing: { pointer: '' } } }"
+    >
+      <template #trailing>
+        <UButton
+          v-show="search !== ''"
+          color="gray"
+          variant="link"
+          icon="i-heroicons-x-mark-20-solid"
+          :padded="false"
+          @click="search = ''"
+        />
+      </template>
+    </UInput>
+  </div>
+</template>

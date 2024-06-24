6c51e9318a4d3e7c76c55615d07148f3a15f0470
Danny <danny.hj.vogel@gmail.com>
2024-06-25 00:06:06 +0200

    Feat: add authModal

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,11 +1,12 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="flex justify-center">
+  <div class="relative flex justify-center items-center">
     <h1
       class="py-1 font-bold text-4xl mx-auto bg-gradient-to-l from-fuchsia-500 via-red-600 to-orange-400 bg-clip-text text-transparent"
     >
       <NuxtLink to="/"> <span class="italic">game</span>State </NuxtLink>
     </h1>
+    <AuthModal />
   </div>
 </template>

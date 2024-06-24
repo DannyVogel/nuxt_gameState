fa5e43fa6503291d2e6ea16730b92f933a2da138
Danny <danny.hj.vogel@gmail.com>
2024-06-25 00:06:18 +0200

    Feat: add list numbers

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -3,33 +3,39 @@ const userStore = useUserStore();
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center gap-20 mt-20">
+  <div class="flex flex-col items-center justify-center gap-12 mt-14">
     <div class="flex flex-col items-center gap-4">
       <h3 class="text-4xl text-primary">Welcome back</h3>
       <h1
         class="text-6xl bg-gradient-to-l from-fuchsia-500 via-red-600 to-orange-400 bg-clip-text text-transparent"
       >
-        {{ userStore.user.name }}
+        {{ userStore.name }}
       </h1>
     </div>
     <div class="flex justify-between gap-12 text-primary">
-      <div class="flex flex-col items-center">
+      <div class="flex flex-col items-center gap-2">
         <img
           class="w-8 h-8"
           src="~/public/icons/gamesToPlay.png"
           alt="shopping bag with controller icon"
         />
-        <p>Games</p>
-        <p>to play</p>
+        <div class="flex flex-col items-center">
+          <p>{{ userStore.getGamesToPlay.length }}</p>
+          <p>Games</p>
+          <p>to play</p>
+        </div>
       </div>
-      <div class="flex flex-col items-center">
+      <div class="flex flex-col items-center gap-2">
         <img
           class="w-8 h-8"
           src="~/public/icons/gamesPlayed.png"
           alt="papers with controller icon"
         />
-        <p>Games</p>
-        <p>played</p>
+        <div class="flex flex-col items-center">
+          <p>{{ userStore.getGamesPlayed.length }}</p>
+          <p>Games</p>
+          <p>played</p>
+        </div>
       </div>
     </div>
   </div>

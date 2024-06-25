a845b30fd86f20f2fb6203d0866c5e4cf8cfab9d
Danny <danny.hj.vogel@gmail.com>
2024-06-26 01:12:27 +0200

    Style: use grid

--- pages/played.vue
+++ pages/played.vue
@@ -15,13 +15,13 @@ onMounted(() => {
   <h3 class="text-2xl font-bold text-center text-primary">Games Played</h3>
   <div class="flex flex-col gap-4">
     <template v-for="year in years">
-      <div class="flex flex-col gap-2">
+      <div class="mt-7 flex flex-col gap-2">
         <h4
           class="w-fit text-xl font-bold text-primary bg-gradient-to-l from-fuchsia-500 via-red-600 to-orange-400 bg-clip-text text-transparent"
         >
           {{ year }}
         </h4>
-        <div class="flex flex-col gap-1">
+        <div class="grid grid-cols-1 gap-1 md:grid-cols-2 lg:grid-cols-3">
           <template
             v-for="game in userStore.getGamesPlayed.filter(
               (game) => game.yearPlayed === year

693ea035f122048635758061180956334664f119
Danny <danny.hj.vogel@gmail.com>
2024-06-26 01:02:28 +0200

    Feat: add view prop to conditionally show text

--- pages/to-play.vue
+++ pages/to-play.vue
@@ -18,7 +18,7 @@ const sortedGames = computed(() => {
     <h3 class="text-2xl font-bold text-center text-primary">Games To Play</h3>
     <div class="mt-10 grid grid-cols-1 gap-1 md:grid-cols-2 lg:grid-cols-3">
       <template v-for="game in sortedGames">
-        <GameCardCompact :game="game" />
+        <GameCardCompact view="to-play" :game="game" />
       </template>
     </div>
   </div>

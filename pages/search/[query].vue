33a46f81818cdc4d928330256bf473445002c5d7
Danny <danny.hj.vogel@gmail.com>
2024-06-26 00:40:00 +0200

    Refactor: update GameCardFull to include buttons and button group

--- pages/search/[query].vue
+++ pages/search/[query].vue
@@ -2,10 +2,13 @@
 // @ts-ignore
 import { AtomSpinner } from "epic-spinners";
 import type { Game } from "~/types/game.interfaces";
+import type { View } from "@/types/common.interfaces";
+
 const route = useRoute();
 const { search } = useSearch();
 const isLoding = ref(true);
 const results = ref<Game[]>([]);
+const view = computed(() => route.name as View);
 
 onMounted(async () => {
   results.value = await search(route.params.query as string);
@@ -32,7 +35,11 @@ onMounted(async () => {
           {{ results.length }} Results for "{{ route.params.query }}"
         </h1>
         <template v-for="game in results">
-          <GameCardFull :game="game" />
+          <GameCardFull :game="game">
+            <template #buttons>
+              <ButtonGroup :view="view" :game="game" />
+            </template>
+          </GameCardFull>
         </template>
       </div>
     </template>

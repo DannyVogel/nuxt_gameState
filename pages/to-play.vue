61718d04df95676ad4f5f089c12874bc07ebdeb9
Danny <danny.hj.vogel@gmail.com>
2024-06-26 00:39:41 +0200

    Refactor: init toPlay list sorted by date and using GameCardCompact

--- pages/to-play.vue
+++ pages/to-play.vue
@@ -1,9 +1,25 @@
 <script lang="ts" setup>
+const userStore = useUserStore();
+const sort = ref("asc");
 
+const sortedGames = computed(() => {
+  return userStore.getGamesToPlay.sort((a, b) => {
+    if (sort.value === "asc") {
+      return a.released.localeCompare(b.released);
+    } else {
+      return b.released.localeCompare(a.released);
+    }
+  });
+});
 </script>
 
 <template>
   <div>
-    
+    <h3 class="text-2xl font-bold text-center text-primary">Games To Play</h3>
+    <div class="mt-10 grid grid-cols-1 gap-1 md:grid-cols-2 lg:grid-cols-3">
+      <template v-for="game in sortedGames">
+        <GameCardCompact :game="game" />
+      </template>
+    </div>
   </div>
 </template>

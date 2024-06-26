09ed299aaa9de9bb6e86604e12143502628c9fee
Danny <danny.hj.vogel@gmail.com>
2024-06-26 22:21:26 +0200

    Style: test new mobile layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div
-    class="container mx-auto min-h-screen max-h-screen grid grid-rows-[56px,1fr,72px]"
-  >
+  <div class="container mx-auto min-h-svh flex flex-col">
     <TheHeader />
-    <div class="p-4 overflow-x-hidden max-h-full">
+    <div class="p-4 flex-grow mb-20">
       <slot />
     </div>
     <TheFooter />

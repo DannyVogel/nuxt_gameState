af36d52217626994288f4c79da4ca3e78b98f89b
Danny <danny.hj.vogel@gmail.com>
2024-06-26 23:24:23 +0200

    Fix: add favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,13 +28,28 @@ export default defineNuxtConfig({
       display: "standalone",
       icons: [
         {
-          src: "/img/gameController.png",
+          src: "/favicon-16x16.png",
+          sizes: "16x16",
+          type: "image/png",
+        },
+        {
+          src: "/favicon-32x32.png",
+          sizes: "32x32",
+          type: "image/png",
+        },
+        {
+          src: "/android-chrome-192x192.png",
           sizes: "192x192",
           type: "image/png",
         },
         {
-          src: "/img/gameController.png",
+          src: "/android-chrome-512x512.png",
           sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "/apple-touch-icon.png",
+          sizes: "128x128",
           type: "image/png",
         },
       ],

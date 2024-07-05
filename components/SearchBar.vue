ff0307022138ca2178d633965af36c21a9ca9193
Danny <danny.hj.vogel@gmail.com>
2024-07-05 23:04:28 +0200

    Refactor: use useFetch composable; removing loading state to searchBar

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,16 +1,33 @@
 <script lang="ts" setup>
+// @ts-ignore
+import { AtomSpinner } from "epic-spinners";
+
 const router = useRouter();
+const isLoading = ref(false);
 const state = reactive({
   search: "",
 });
-const doSearch = async (e: Event) => {
+
+const doSearch = async () => {
+  isLoading.value = true;
   router.push("/search/" + state.search);
 };
 </script>
 
 <template>
   <div class="max-w-xs mx-auto">
-    <UForm :state="state" @submit="doSearch">
+    <div v-if="isLoading" class="mx-auto">
+      <atom-spinner
+        :animation-duration="1000"
+        :size="50"
+        :color="'rgb(59 130 246 / 0.5)'"
+        class="mx-auto"
+      />
+      <p class="mt-1 text-primary text-sm animate-pulse text-center">
+        Searching...
+      </p>
+    </div>
+    <UForm v-else :state="state" @submit="doSearch">
       <UInput
         v-model="state.search"
         name="q"
--- pages/search/[query].vue
+++ pages/search/[query].vue
@@ -1,48 +1,48 @@
 <script lang="ts" setup>
-// @ts-ignore
-import { AtomSpinner } from "epic-spinners";
 import type { Game } from "~/types/game.interfaces";
 import type { View } from "@/types/common.interfaces";
 
 const route = useRoute();
-const { search } = useSearch();
-const isLoading = ref(true);
 const results = ref<Game[]>([]);
 const view = computed(() => route.name as View);
 
-onMounted(async () => {
-  results.value = await search(route.params.query as string);
-  isLoading.value = false;
+const resultsText = computed(() => {
+  return results.value.length === 0
+    ? `No results found for "${route.params.query}"`
+    : results.value.length === 1
+    ? `1 result for "${route.params.query}"`
+    : `${results.value.length} results for "${route.params.query}"`;
+});
+
+await useFetch(`/api/search/${route.params.query}`, {
+  onResponse: ({ response }) => {
+    if (response.status === 200) {
+      results.value = response._data.body;
+    } else {
+      results.value = [];
+    }
+  },
 });
 </script>
 
 <template>
   <div class="min-h-full flex flex-col items-center justify-center">
-    <atom-spinner
-      v-if="isLoading"
-      :animation-duration="1000"
-      :size="100"
-      :color="'rgb(59 130 246 / 0.5)'"
-      class="mt-20"
-    />
-    <template v-else>
-      <div v-if="results.length === 0">
-        <p class="text-primary">
-          No results found for "{{ route.params.query }}"
-        </p>
-      </div>
-      <div v-else class="flex flex-col gap-4">
-        <h1 class="text-xl font-bold text-primary text-center">
-          {{ results.length }} Results for "{{ route.params.query }}"
-        </h1>
-        <template v-for="game in results">
-          <GameCardFull :view="view" :game="game">
-            <template #buttons>
-              <ButtonGroup :view="view" :game="game" />
-            </template>
-          </GameCardFull>
-        </template>
-      </div>
-    </template>
+    <div v-if="results.length === 0">
+      <p class="text-primary">
+        No results found for "{{ route.params.query }}"
+      </p>
+    </div>
+    <div v-else class="flex flex-col gap-4">
+      <h1 class="text-xl font-bold text-primary text-center">
+        {{ resultsText }}
+      </h1>
+      <template v-for="game in results">
+        <GameCardFull :view="view" :game="game">
+          <template #buttons>
+            <ButtonGroup :view="view" :game="game" />
+          </template>
+        </GameCardFull>
+      </template>
+    </div>
   </div>
 </template>

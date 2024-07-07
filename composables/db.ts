59ba25063957b0c6539ee9f889ef2d37675f9e36
Danny <danny.hj.vogel@gmail.com>
2024-07-07 17:23:14 +0200

    Chore: remove unused

--- composables/db.ts
+++ composables/db.ts
@@ -1,12 +1,9 @@
-import { ref as fbRef, get, update, remove } from "firebase/database";
 import type { Game } from "~/types/game.interfaces";
 
 export const useDb = () => {
-  const { $db, $gameStateDB } = useNuxtApp();
   const userStore = useUserStore();
 
   const addToList = async (game: Game) => {
-    console.log("add to list game", game);
     try {
       const res = await $fetch("/api/game-list/add", {
         method: "POST",
@@ -19,7 +16,6 @@ export const useDb = () => {
   };
 
   const removeFromList = async (gameID: Game["id"]) => {
-    console.log("remove from list gameID", gameID);
     try {
       const res = await $fetch("/api/game-list/remove", {
         method: "DELETE",

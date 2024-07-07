bfa529d66872c3798b3aa6a26bd92ed9eda01429
Danny <danny.hj.vogel@gmail.com>
2024-07-07 16:12:57 +0200

    WIP: refactor to use db in backend

--- composables/db.ts
+++ composables/db.ts
@@ -5,32 +5,33 @@ export const useDb = () => {
   const { $db, $gameStateDB } = useNuxtApp();
   const userStore = useUserStore();
 
-  const getGameList = async (UID: string) => {
-    const listRef = fbRef($db, `gameState/users/${UID}/gameList`);
-    const snapshot = await get(listRef);
-    if (snapshot.exists()) {
-      return Object.values(snapshot.val());
-    } else {
-      return [];
+  const addToList = async (game: Game) => {
+    console.log("add to list game", game);
+    try {
+      const res = await $fetch("/api/game-list/add", {
+        method: "POST",
+        body: game,
+      });
+      console.log("add to list res", res);
+    } catch (error) {
+      console.error("add to list error", error);
     }
   };
 
-  const addToList = async (game: Game) => {
-    const updates: { [key: string]: Game } = {};
-    updates[`/users/${userStore.UID}/gameList/${game.id}`] = game;
-    update($gameStateDB, updates);
-  };
-
   const removeFromList = async (gameID: Game["id"]) => {
-    const gameRef = fbRef(
-      $db,
-      `gameState/users/${userStore.UID}/gameList/${gameID}`
-    );
-    remove(gameRef);
+    console.log("remove from list gameID", gameID);
+    try {
+      const res = await $fetch("/api/game-list/remove", {
+        method: "DELETE",
+        body: { id: gameID },
+      });
+      console.log("remove from list res", res);
+    } catch (error) {
+      console.error("remove from list error", error);
+    }
   };
 
   return {
-    getGameList,
     addToList,
     removeFromList,
   };

c5ab3b0a7df50252c7c36101c624570ae1470a9f
dvogelg4al <134928350+dvogelg4al@users.noreply.github.com>
2024-07-07 20:00:49 +0200

    Fix: add user to routes needing token verification

--- server/middleware/verify-token.ts
+++ server/middleware/verify-token.ts
@@ -2,7 +2,10 @@ import { verifyToken } from "../services/tokenManagers/auth";
 
 export default defineEventHandler(async (event) => {
   console.log("request path", event.path);
-  if (event.path.includes("/api/game-list")) {
+  if (
+    event.path.includes("/api/game-list") ||
+    event.path.includes("/api/user")
+  ) {
     const token = getCookie(event, "token");
     if (token) {
       const decodedToken = await verifyToken(token);

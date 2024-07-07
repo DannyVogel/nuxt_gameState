d4875b6d08dd7f2f53ec130534fa84ef16579642
Danny <danny.hj.vogel@gmail.com>
2024-07-07 23:51:48 +0200

    WIP: refresh token attempt

--- server/middleware/verify-token.ts
+++ server/middleware/verify-token.ts
@@ -1,7 +1,7 @@
 import { verifyToken } from "../services/tokenManagers/auth";
+import { auth } from "../services/firebase";
 
 export default defineEventHandler(async (event) => {
-  console.log("request path", event.path);
   if (
     event.path.includes("/api/game-list") ||
     event.path.includes("/api/user")
@@ -14,11 +14,20 @@ export default defineEventHandler(async (event) => {
         decodedToken,
       };
     } else {
-      throw createError({
-        statusCode: 401,
-        statusMessage: "Unauthorized",
-        message: "No token provided",
-      });
+      const newToken = await auth.currentUser?.getIdToken(true);
+      if (newToken) {
+        setCookie(event, "token", newToken, { httpOnly: true });
+        const decodedToken = await verifyToken(newToken);
+        event.context = {
+          ...event.context,
+          decodedToken,
+        };
+      } else
+        throw createError({
+          statusCode: 401,
+          statusMessage: "Unauthorized",
+          message: "No token provided",
+        });
     }
   }
 });

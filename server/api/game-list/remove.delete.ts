d57f60cd5b8da09e1cff0d5ba1dafeee47e250bd
Danny <danny.hj.vogel@gmail.com>
2024-07-07 17:41:29 +0200

    Fix: get and return gameList on add or remove

--- server/api/game-list/remove.delete.ts
+++ server/api/game-list/remove.delete.ts
@@ -1,8 +1,24 @@
 import { DbController } from "~/server/controllers/db.controller";
+import { GameListPayload, NitroResponse } from "~/types/auth.interfaces";
+import { UserGame } from "~/types/game.interfaces";
 
-export default defineEventHandler(async (event) => {
-  const game = await readBody(event);
-  const { decodedToken } = event.context;
+export default defineEventHandler(
+  async (event): Promise<NitroResponse<GameListPayload>> => {
+    const game = await readBody(event);
+    const { decodedToken } = event.context;
 
-  const res = await DbController.removeFromList(decodedToken.uid, game.id);
-});
+    try {
+      await DbController.removeFromList(decodedToken.uid, game.id);
+      const updatedList = (await DbController.getGameList(
+        decodedToken.uid
+      )) as UserGame[];
+      return serverResponse(200, "Ok", updatedList);
+    } catch (error: any) {
+      throw createError({
+        statusCode: 500,
+        statusMessage: "Internal Server Error",
+        message: error.message,
+      });
+    }
+  }
+);

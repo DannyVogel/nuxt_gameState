be355e229e4421e6717a5d3bd9a50861630a1836
Danny <danny.hj.vogel@gmail.com>
2024-07-08 08:08:37 +0200

    Fix: add error handling

--- server/api/game-list/index.get.ts
+++ server/api/game-list/index.get.ts
@@ -2,6 +2,15 @@ import { DbController } from "~/server/controllers/db.controller";
 
 export default defineEventHandler(async (event) => {
   const { decodedToken } = event.context;
-  const res = await DbController.getGameList(decodedToken.uid);
-  return res;
+  try {
+    const res = await DbController.getGameList(decodedToken.uid);
+    return res;
+  } catch (error: any) {
+    console.error("Error getting game list:", error);
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Internal Server Error",
+      message: error.message,
+    });
+  }
 });

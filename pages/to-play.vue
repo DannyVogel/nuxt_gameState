3b53bb16aae5dd4e54ed45d9208b20fad260f8f9
DannyVogel <danny.hj.vogel@gmail.com>
2024-09-16 23:50:57 +0200

    Fix: reactive list

--- pages/to-play.vue
+++ pages/to-play.vue
@@ -1,15 +1,12 @@
 <script lang="ts" setup>
-import type { UserGame } from "~/types/game.interfaces";
-
 const db = useDb();
-const gamesToPlay = ref<UserGame[]>(db.getGamesToPlay.value);
 </script>
 
 <template>
   <div>
     <h3 class="text-2xl font-bold text-center text-primary">Games To Play</h3>
     <div class="mt-10 grid grid-cols-1 gap-1 md:grid-cols-2 lg:grid-cols-3">
-      <template v-for="game in gamesToPlay">
+      <template v-for="game in db.getGamesToPlay.value">
         <GameCardCompact view="to-play" :game="game" />
       </template>
     </div>

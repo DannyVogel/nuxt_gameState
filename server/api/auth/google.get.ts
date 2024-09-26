9fefc0abb8048b8afa487d6ff35dff205688bd20
DannyVogel <danny.hj.vogel@gmail.com>
2024-09-26 22:15:49 +0200

    Feat: google oauth login

---
+++ server/api/auth/google.get.ts
@@ -0,0 +1,15 @@
+const { googleRedirectURL } = useRuntimeConfig();
+
+export default oauthGoogleEventHandler({
+  config: {
+    redirectURL: googleRedirectURL || undefined,
+  },
+  async onSuccess(event, { user, tokens }) {
+    await setUserSession(event, {
+      user,
+      secure: tokens.id_token,
+      loggedInAt: new Date(),
+    });
+    return sendRedirect(event, "/");
+  },
+});

96581a417f83fe147c7d1d886cdc6b68c2ba13a4
DannyVogel <danny.hj.vogel@gmail.com>
2024-09-26 22:16:28 +0200

    Refactor: use nuxt auth utils

--- components/Auth/LoggedOut.vue
+++ components/Auth/LoggedOut.vue
@@ -1,34 +1,9 @@
 <script setup lang="ts">
 const emit = defineEmits(["close"]);
-const fbAuth = useFBAuth();
-
-const state = reactive({
-  email: "",
-  password: "",
-});
-const errorMessage = ref("");
-
-async function onSubmit(event: Event) {
-  if (!state.email || !state.password) {
-    errorMessage.value = "Please fill in all fields";
-    return;
-  }
-  await fbAuth.login(state.email, state.password);
-  state.email = "";
-  state.password = "";
-}
 </script>
 
 <template>
-  <UForm :state="state" class="space-y-4 p-4" @submit="onSubmit">
-    <UFormGroup size="xl" label="Email" name="email">
-      <UInput size="xl" v-model="state.email" />
-    </UFormGroup>
-
-    <UFormGroup size="xl" label="Password" name="password">
-      <UInput size="xl" v-model="state.password" type="password" />
-    </UFormGroup>
-
-    <UButton type="submit">Submit</UButton>
-  </UForm>
+  <div class="flex items-center justify-center my-10">
+    <NuxtLink to="/api/auth/google" external>Login with Google</NuxtLink>
+  </div>
 </template>

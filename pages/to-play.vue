1a0772e116550773174e4fec364251fcd3c3ca43
DannyVogel <danny.hj.vogel@gmail.com>
2025-02-15 01:55:02 +0100

    Refactor: games to play list fetching logic

--- pages/to-play.vue
+++ pages/to-play.vue
@@ -1,12 +1,16 @@
 <script lang="ts" setup>
-const listStore = useListStore();
+const listStore = useGameList();
+
+const { data: gamesToPlay } = await useAsyncData("getGamesToPlay", () =>
+  listStore.getGamesToPlay()
+);
 </script>
 
 <template>
   <div>
     <h3 class="text-2xl font-bold text-center text-primary">Games To Play</h3>
     <div class="mt-10 grid grid-cols-1 gap-1 md:grid-cols-2 lg:grid-cols-3">
-      <template v-for="game in listStore.gamesToPlay">
+      <template v-for="game in gamesToPlay">
         <GameCardCompact view="to-play" :game="game" />
       </template>
     </div>

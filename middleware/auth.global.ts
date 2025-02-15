cde8b0710b6b2be1211608f72011ceb09fb8f37f
DannyVogel <danny.hj.vogel@gmail.com>
2025-02-15 01:30:50 +0100

    Chore: remove logs

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,8 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const { loggedIn } = useUserSession();
-  console.log("loggedIn", loggedIn.value);
   if (!loggedIn.value && to.path !== "/") {
-    console.log("Not logged in, redirecting to /");
     return navigateTo({ path: "/", query: { auth: "true" } });
   }
 });

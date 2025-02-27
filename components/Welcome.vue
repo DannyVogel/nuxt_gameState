9614a7555b2a117f0fe2be900903b6d98f0e5118
DannyVogel <danny.hj.vogel@gmail.com>
2025-02-28 00:04:58 +0100

    Refactor: Simplify Welcome component and update game stats link

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -17,11 +17,11 @@ const { data: gamesCount } = await useAsyncData(
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center gap-12 mt-10">
+  <div
+    class="flex flex-col items-center justify-center gap-12 mt-10 text-primary"
+  >
     <div class="flex flex-col items-center gap-4">
-      <h3 class="text-4xl text-primary">
-        Welcome {{ loggedIn ? "back" : "" }}
-      </h3>
+      <h3 class="text-4xl">Welcome {{ loggedIn ? "back" : "" }}</h3>
       <h1
         v-if="loggedIn"
         class="text-6xl bg-gradient-to-l from-fuchsia-500 via-red-600 to-orange-400 bg-clip-text text-transparent"
@@ -57,27 +57,13 @@ const { data: gamesCount } = await useAsyncData(
           <p>played</p>
         </div>
       </NuxtLink>
-      <NuxtLink to="/stats" class="flex flex-col items-center gap-2">
-        <svg
-          class="w-8 h-8"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M8 13V17M16 11V17M12 7V17M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-        <div class="flex flex-col items-center">
-          <p>Game</p>
-          <p>Stats</p>
-        </div>
-      </NuxtLink>
     </div>
+    <NuxtLink to="/stats" class="flex items-center gap-2">
+      <UIcon name="i-ph-chart-bar-duotone" class="w-8 h-8 text-white" />
+      <div class="flex flex-col items-center">
+        <p>Game Stats</p>
+      </div>
+    </NuxtLink>
     <p v-if="!loggedIn" class="text-center text-primary max-w-xs px-4">
       <NuxtLink to="/api/auth/google" external class="underline text-white"
         >Login</NuxtLink

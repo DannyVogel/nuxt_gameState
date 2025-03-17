a796a7636b6929a5bd86e5718c6aa5a45c5942e0
DannyVogel <danny.hj.vogel@gmail.com>
2025-03-17 21:04:56 +0100

    Style: Restrict layout width to max-w-screen-lg for improved responsiveness

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="container mx-auto min-h-svh flex flex-col">
+  <div class="container mx-auto min-h-svh flex flex-col max-w-screen-lg">
     <TheHeader />
     <div class="p-4 flex-grow mb-20">
       <slot />

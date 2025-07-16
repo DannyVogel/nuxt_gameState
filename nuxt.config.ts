f43bde8d039c6912a871aecaa339f1cc487ed147
DannyVogel <danny.hj.vogel@gmail.com>
2025-07-16 23:14:17 +0200

    Feat: Add app head configuration with title and favicon for improved SEO and user experience

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "gameState",
+      htmlAttrs: {
+        lang: "en",
+      },
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
   tailwindcss: {
     viewer: false,
   },
